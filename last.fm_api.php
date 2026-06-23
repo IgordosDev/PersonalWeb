255ca06f4b984e3f4a17dd8e39b21ad4d8c3f4fb
IgordosDev <igordos@vivaldi.net>
2026-06-24 01:53:02 +0300

    замена скрипта отображения, видос новый и фикс апи ласт фм

--- last.fm_api.php
+++ last.fm_api.php
@@ -1,14 +1,25 @@
 <?php
-    $curlSession = curl_init();
-    $api_user = $_ENV["lfm.user"];
-    $api_key = $_ENV["lfm.key"];
-    curl_setopt($curlSession, CURLOPT_URL, "https://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=$api_user&api_key=$api_key&format=json&limit=1");
-    curl_setopt($curlSession, CURLOPT_BINARYTRANSFER, true);
-    curl_setopt($curlSession, CURLOPT_RETURNTRANSFER, true);
-    
-    $jsonData = json_decode(curl_exec($curlSession));
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=utf-8");
+$curlSession = curl_init();
+$api_user = $_ENV["lfm.user"];
+$api_key = $_ENV["lfm.key"];
+$url = "https://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=$api_user&api_key=$api_key&format=json&limit=1";
+curl_setopt($curlSession, CURLOPT_URL, $url);
+curl_setopt($curlSession, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($curlSession, CURLOPT_TIMEOUT, 10); 
+
+$response = curl_exec($curlSession);
+if ($response === false) {
+    http_response_code(500);
+    echo json_encode([
+        "error" => true,
+        "message" => curl_error($curlSession)
+    ]);
     curl_close($curlSession);
-    
-    header("Content-Type: application/json");
-    echo json_encode($jsonData);
+    exit;
+}
+
+curl_close($curlSession);
+echo $response;
 ?>
